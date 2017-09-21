2d4fbffb6c7590c92bd75496571ea332df609c24
Adam-Shahzad <adam.shahzad@outlook.com>
2017-09-21 16:44:54 +0100

    screening times

--- app/feature/BuyTicket/filmcontroller.js
+++ app/feature/BuyTicket/filmcontroller.js
@@ -1,17 +1,24 @@
 (function () {
 
     var filmcontroller = function ($scope, $http, myService) {
+        $scope.sortby = 'title';
+        $scope.reverse = false;
 
         console.log(myService.get());
         let movie = myService.get();
 
         $scope.filmToDisp = JSON.parse(movie);
 
-        $http.get('https://raw.githubusercontent.com/PAchilleos/CinemaWebsite/Developer/films.json')
+        $http.get('https://raw.githubusercontent.com/PAchilleos/CinemaWebsite/adam/screening.json')
             .then(function (result) {
-                $scope.movies = result.data;
-
+                $scope.screenings = result.data;
+                console.log($scope.screenings);
             });
+
+        $scope.doSort = function (propName) {
+            $scope.sortBy = propName;
+            $scope.reverse = !$scope.reverse
+        };
 
     };
 
